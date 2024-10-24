b96d5f39316619456aa0768e810ad71a086c7362
Pijo <danielsikora100@gmail.com>
2024-10-24 09:13:13 +0200

    dodaniue formularzu logowania

---
+++ src/components/LogForm.jsx
@@ -0,0 +1,21 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus aperiam voluptatum quam vitae optio fuga quos hic reprehenderit laboriosam eligendi excepturi omnis harum laborum, unde error expedita earum quia.
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, dolore!</p>
+        <ul>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            
+        </ul>
+    </p>
+</body>
+</html>
